18ac5fa2a4f05f349570350f57a58515e3984d20
HeitorBMVanguard <heitorborba@sistemavanguard.com.br>
2024-03-09 13:15:33 -0300

    Add loading estático para todas as páginas da aplicação

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import Loading from "./components/Loading"
+
+export default function Loadingdeafult(){
+    return(
+        <div className="flex items-center content-center
+        w-screen h-screen">
+            <Loading/>
+        </div>
+    )
+}

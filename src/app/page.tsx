b84f6b3a5629999817a6a1a496215d0b644a8199
HeitorBMVanguard <heitorborba@sistemavanguard.com.br>
2024-03-11 12:34:23 -0300

    Add resposividade nas paginas /page e search-users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,26 +18,24 @@ export default async function Home() {
   const dataRepos : GithubReposType[] = await responseRepos.json();
 
   return (
-    <main className="flex justify-between w-full">
+    <main className="flex flex-col md:flex-row justify-between w-full">
       <UserProfile isMyProfile profile={data}/>
-      <div className="flex flex-col gap-4 w-full">
-      <div className="flex justify-end mr-6 mt-4"> 
-      <Link href={'/search-users'} className="inline-block" passHref>
-        
-          <button className="w-full h-12 px-4 py-2 rounded-lg bg-green-800 text-white text-base font-bold cursor-pointer border-2
-           border-black hover:opacity-90">
-            Encontrar Usuários
-          </button>
-        
-      </Link>
-    </div>
-        <div className="grid grid-cols-2 gap-4 place-items-center md:flex-col">
-        {dataRepos.slice(0, 6).map((repo) => (
-          <RepositoryCard  repo={repo} key={repo.id}/>
-        ))}
-          
+      <div className="flex flex-col gap-4 w-full px-2 md:px-0">
+        <div className="flex justify-end mr-0 md:mr-6 mt-4"> 
+          <Link href={'/search-users'} className="inline-block w-full sm:w-auto" passHref>
+            <button className="w-full h-12 px-4 py-2 rounded-lg bg-green-800 text-white text-base font-bold cursor-pointer border-2 border-black hover:opacity-90">
+              Encontrar Usuários
+            </button>
+          </Link>
+        </div>
+        <div className="flex flex-col gap-4 md:grid md:grid-cols-2 md:gap-4 place-items-center">
+          {dataRepos.slice(0, 6).map((repo) => (
+            <RepositoryCard repo={repo} key={repo.id}/>
+          ))}
         </div>
       </div>
     </main>
   );
+  
+  
 }
--- src/app/search-users/page.tsx
+++ src/app/search-users/page.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import { useState } from "react";
 
@@ -17,37 +17,34 @@ export default function Home() {
 
   const [repos, setRepos] = useState<GithubReposType[]>([]);
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
-  async function handleSearchUser()  {
-    
+  async function handleSearchUser() {
     if (!searchInput) {
       setUser(null);
       return;
     }
 
-  
-    try{
+    try {
       setLoading(true);
 
       const response = await fetch(
-      `https://api.github.com/users/${searchInput}`
+        `https://api.github.com/users/${searchInput}`
       );
-    
-      const data : GithubProfileType = await response.json();
-    
+
+      const data: GithubProfileType = await response.json();
+
       setUser(data);
-  
+
       const responseRepos = await fetch(
-      `https://api.github.com/users/${data.login}/repos`);
-    
-      const dataRepos : GithubReposType[] = await responseRepos.json();
-  
+        `https://api.github.com/users/${data.login}/repos`
+      );
+
+      const dataRepos: GithubReposType[] = await responseRepos.json();
+
       setRepos(dataRepos.slice(0, 6));
-
-    } catch (error){
+    } catch (error) {
       console.log(error);
-
     } finally {
       setLoading(false);
     }
@@ -56,44 +53,39 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center w-full">
       {/* A barra de pesquisa e o botão, agora dentro de uma div centralizada para manter a consistência */}
-      <div className="w-full flex justify-center mt-4">
+      <div className="w-full flex justify-center mt-4 px-4 md:px-0">
         <input
           type="text"
           placeholder="Digite o nome do usuário..."
           value={searchInput}
           onChange={(event) => setSearchInput(event.target.value)}
-          className="text-black p-2 w-80 rounded-lg border-2 border-gray-300"
+          className="text-black p-2 w-full md:w-80 rounded-lg border-2 border-gray-300"
         />
-        <button  onClick={handleSearchUser} className="ml-4 h-12 px-4 py-2 rounded-lg bg-green-800 text-white text-base font-bold cursor-pointer border-2 border-black hover:opacity-90"
-       >
+        <button onClick={handleSearchUser} className="ml-4 h-12 px-4 py-2 rounded-lg bg-green-800 text-white text-base font-bold cursor-pointer border-2 border-black hover:opacity-90">
           Pesquisar
         </button>
       </div>
-
-      { loading ? (
-      <Loading/>
-      ):  user?.name ?(
-
-      <div className="flex justify-between w-full mt-8">
-        <UserProfile profile={user}/>
-        <div className="flex flex-col gap-4 w-full max-w-4xl ml-auto">
-        <div className="grid grid-cols-2 gap-4 place-items-center md:flex-col">
-
-            {repos.map((repo) => (
-              <RepositoryCard repo={repo} key={repo.id}/>
-            ))}
-            
-           
+  
+      {loading ? (
+        <Loading/>
+      ) : user?.name ? (
+        <div className="flex flex-col md:flex-row justify-between w-full mt-8 px-4 md:px-0">
+          <UserProfile profile={user}/>
+          <div className="flex flex-col gap-4 w-full max-w-4xl ml-auto">
+            <div className="flex flex-col md:grid md:grid-cols-2 gap-4 place-items-center">
+              {repos.map((repo) => (
+                <RepositoryCard repo={repo} key={repo.id}/>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-    ) : (
-     
-      <div className="flex flex-col items-center justify-center w-full mt-8">
-        <BiSolidError size={100} className="text-red-700" />
-        <h3 className="text-gray-500 text-2xl font-bold">Nenhum usuário encontrado</h3>
-      </div>
-    )}
+      ) : (
+        <div className="flex flex-col items-center justify-center w-full mt-8">
+          <BiSolidError size={100} className="text-red-700" />
+          <h3 className="text-gray-500 text-2xl font-bold">Nenhum usuário encontrado</h3>
+        </div>
+      )}
     </div>
   );
+  
 }
